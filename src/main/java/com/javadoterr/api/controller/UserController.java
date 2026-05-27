8300c42f3c6ae2b5bdf0472c37e5e6adba2be08a
javadoterr <javadoterr@gmail.com>
2026-05-27 23:25:35 +0530

    log added in controller

--- src/main/java/com/javadoterr/api/controller/UserController.java
+++ src/main/java/com/javadoterr/api/controller/UserController.java
@@ -2,6 +2,7 @@ package com.javadoterr.api.controller;
 
 import com.javadoterr.api.bean.User;
 import com.javadoterr.api.service.DataGenService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/users", produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
 public class UserController {
 
     private DataGenService dataGenService;
@@ -22,11 +24,13 @@ public class UserController {
     public ResponseEntity<List<User>> getAllUsers(@PathVariable(name = "count") int count,
                                                   @RequestParam(name = "easy", defaultValue = "false") boolean easy){
         List<User> users = dataGenService.getAllUsers(count, easy);
+        log.info("CONTROLLER : getAllUsers response ");
         return ResponseEntity.ok().body(users);
     }
 
     @GetMapping(path = "/one")
     public ResponseEntity<User> getUser(){
+        log.info("CONTROLLER : getUser response ");
         return ResponseEntity.ok()
                 .body(dataGenService.randomUserViaDatafaker());
     }
